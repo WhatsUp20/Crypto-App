266a0f771195e78466175fe4be46b529a6bff4fa
Alex Nazarov <storyofspolding@gmail.com>
2021-02-10 22:11:25 +0300

    add and configured viewModel

--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.cryptoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.api.ApiFactory
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -10,24 +12,15 @@ import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("testOfLoadingData", it.toString())
-            }, {
-                Log.d("testOfLoadingData", it.message.toString())
-            })
-        compositeDisposable.add(disposable)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(CoinViewModel::class.java)
+        viewModel.loadData()
+        viewModel.priceList.observe(this, Observer {
+            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        })
     }
 }
--- app/src/main/java/com/example/cryptoapp/pojo/CoinPriceInfo.kt
+++ app/src/main/java/com/example/cryptoapp/pojo/CoinPriceInfo.kt
@@ -1,169 +1,171 @@
 package com.example.cryptoapp.pojo
 
+
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
-
 import com.google.gson.annotations.SerializedName
 
+
 @Entity(tableName = "full_price_list")
-data class CoinPriceInfo (
+data class CoinPriceInfo(
 
     @SerializedName("TYPE")
     @Expose
-     val TYPE: String? = null,
+    val TYPE: String? = null,
 
     @SerializedName("MARKET")
     @Expose
-     val MARKET: String? = null,
+    val MARKET: String? = null,
+
     @PrimaryKey
     @SerializedName("FROMSYMBOL")
     @Expose
-     val FROMSYMBOL: String? = null,
+    val FROMSYMBOL: String,
 
     @SerializedName("TOSYMBOL")
     @Expose
-     val TOSYMBOL: String? = null,
+    val TOSYMBOL: String? = null,
 
     @SerializedName("FLAGS")
     @Expose
-     val FLAGS: String? = null,
+    val FLAGS: String? = null,
 
     @SerializedName("PRICE")
     @Expose
-     val PRICE: Double = 0.0,
+    val PRICE: Double = 0.0,
 
     @SerializedName("LASTUPDATE")
     @Expose
-     val LASTUPDATE: Int = 0,
+    val LASTUPDATE: Int = 0,
 
     @SerializedName("LASTVOLUME")
     @Expose
-     val LASTVOLUME: Double = 0.0,
+    val LASTVOLUME: Double = 0.0,
 
     @SerializedName("LASTVOLUMETO")
     @Expose
-     val LASTVOLUMETO: Double = 0.0,
+    val LASTVOLUMETO: Double = 0.0,
 
     @SerializedName("LASTTRADEID")
     @Expose
-     val LASTTRADEID: String? = null,
+    val LASTTRADEID: String? = null,
 
     @SerializedName("VOLUMEDAY")
     @Expose
-     val VOLUMEDAY: Double = 0.0,
+    val VOLUMEDAY: Double = 0.0,
 
     @SerializedName("VOLUMEDAYTO")
     @Expose
-     val VOLUMEDAYTO: Double = 0.0,
+    val VOLUMEDAYTO: Double = 0.0,
 
     @SerializedName("VOLUME24HOUR")
     @Expose
-     val VOLUME24HOUR: Double = 0.0,
+    val VOLUME24HOUR: Double = 0.0,
 
     @SerializedName("VOLUME24HOURTO")
     @Expose
-     val VOLUME24HOURTO: Double = 0.0,
+    val VOLUME24HOURTO: Double = 0.0,
 
     @SerializedName("OPENDAY")
     @Expose
-     val OPENDAY: Double = 0.0,
+    val OPENDAY: Double = 0.0,
 
     @SerializedName("HIGHDAY")
     @Expose
-     val HIGHDAY: Double = 0.0,
+    val HIGHDAY: Double = 0.0,
 
     @SerializedName("LOWDAY")
     @Expose
-     val LOWDAY: Double = 0.0,
+    val LOWDAY: Double = 0.0,
 
     @SerializedName("OPEN24HOUR")
     @Expose
-     val OPEN24HOUR: Double = 0.0,
+    val OPEN24HOUR: Double = 0.0,
 
     @SerializedName("HIGH24HOUR")
     @Expose
-     val HIGH24HOUR: Double = 0.0,
+    val HIGH24HOUR: Double = 0.0,
 
     @SerializedName("LOW24HOUR")
     @Expose
-     val LOW24HOUR: Double = 0.0,
+    val LOW24HOUR: Double = 0.0,
 
     @SerializedName("LASTMARKET")
     @Expose
-     val LASTMARKET: String? = null,
+    val LASTMARKET: String? = null,
 
     @SerializedName("VOLUMEHOUR")
     @Expose
-     val VOLUMEHOUR: Double = 0.0,
+    val VOLUMEHOUR: Double = 0.0,
 
     @SerializedName("VOLUMEHOURTO")
     @Expose
-     val VOLUMEHOURTO: Double = 0.0,
+    val VOLUMEHOURTO: Double = 0.0,
 
     @SerializedName("OPENHOUR")
     @Expose
-     val OPENHOUR: Double = 0.0,
+    val OPENHOUR: Double = 0.0,
 
     @SerializedName("HIGHHOUR")
     @Expose
-     val HIGHHOUR: Double = 0.0,
+    val HIGHHOUR: Double = 0.0,
 
     @SerializedName("LOWHOUR")
     @Expose
-     val LOWHOUR: Double = 0.0,
+    val LOWHOUR: Double = 0.0,
 
     @SerializedName("TOPTIERVOLUME24HOUR")
     @Expose
-     val TOPTIERVOLUME24HOUR: Double = 0.0,
+    val TOPTIERVOLUME24HOUR: Double = 0.0,
 
     @SerializedName("TOPTIERVOLUME24HOURTO")
     @Expose
-     val TOPTIERVOLUME24HOURTO: Double = 0.0,
+    val TOPTIERVOLUME24HOURTO: Double = 0.0,
 
     @SerializedName("CHANGE24HOUR")
     @Expose
-     val CHANGE24HOUR: Double = 0.0,
+    val CHANGE24HOUR: Double = 0.0,
 
     @SerializedName("CHANGEPCT24HOUR")
     @Expose
-     val CHANGEPCT24HOUR: Double = 0.0,
+    val CHANGEPCT24HOUR: Double = 0.0,
 
     @SerializedName("CHANGEDAY")
     @Expose
-     val CHANGEDAY: Double = 0.0,
+    val CHANGEDAY: Double = 0.0,
 
     @SerializedName("CHANGEPCTDAY")
     @Expose
-     val CHANGEPCTDAY: Double = 0.0,
+    val CHANGEPCTDAY: Double = 0.0,
 
 
     @SerializedName("SUPPLY")
     @Expose
-     val CUPPLY: Int = 0,
+    val CUPPLY: Int = 0,
 
     @SerializedName("MKTCAP")
     @Expose
-     val MKTCAP: Double = 0.0,
+    val MKTCAP: Double = 0.0,
 
 
     @SerializedName("TOTALVOLUME24H")
     @Expose
-     val TOTALVOLUME24H: Double = 0.0,
+    val TOTALVOLUME24H: Double = 0.0,
 
     @SerializedName("TOTALVOLUME24HTO")
     @Expose
-     val TOTALVOLUME24HTO: Double = 0.0,
+    val TOTALVOLUME24HTO: Double = 0.0,
 
     @SerializedName("TOTALTOPTIERVOLUME24H")
     @Expose
-     val TOTALTOPTIERVOLUME24H: Double = 0.0,
+    val TOTALTOPTIERVOLUME24H: Double = 0.0,
 
     @SerializedName("TOTALTOPTIERVOLUME24HTO")
     @Expose
-     val TOTALTOPTIERVOLUME24HTO: Double = 0.0,
+    val TOTALTOPTIERVOLUME24HTO: Double = 0.0,
 
     @SerializedName("IMAGEURL")
     @Expose
-     val IMAGEURL: String? = null
+    val IMAGEURL: String? = null
 )
